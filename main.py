53559d7c51683810d031913a83ce8b779ef2a035
Eugene Park <jsukpark@users.noreply.github.com>
2023-12-07 17:14:19 -0600

    Encapsulate iLQR verification code into a function
    - In case this is useful later

--- main.py
+++ main.py
@@ -1,67 +1,120 @@
 """Main code
 """
 
+from typing import Tuple
 from functools import partial
 
 import jax.numpy as np
-from jax import random
 from trajax.optimizers import ilqr
 import matplotlib.pyplot as plt
 
 
-def example_cost(x, u, t) -> float:
-    """A cost function just for code verification
+def verify_ilqr():
+    """
+    Verify that `trajax.optimizers.ilqr()` works properly.
+
+    We consider a toy system,
+    a 1D simple harmonic oscillator around x = 0
+    with mass = 1 and k (spring constant) = 1,
+    where control u is the external force applied along the x-axis.
+
+    Such system has the following dynamics:
+    ```
+    \ddot{q} = -q + u/mass
+    ```
+
+    We define a state to be a 3-tuple `(q, \dot{q}, \ddot{q})` of
+    position, velocity, and acceleration.
+    Then the next state is computed using velocity Verlet algorithm
+    (https://en.wikipedia.org/wiki/Verlet_integration#Velocity_Verlet)
+    """
+    def example_cost(x: np.ndarray, u: np.ndarray, t: float) -> float:
+        """Cost function to minimize
+        for the 1D simple harmonic oscillator
+        (must also minimize the amount of force applied)
+        """
+        C = np.array([[1, 0, 0], [0, 0, 0], [0, 0, 0]]) * 0.5
+        return x @ C @ x + u @ u
+
+    def _dyn(
+            x: Tuple, u: float,
+            dt: float = 1e-2, mass: float = 1.0,
+            ) -> Tuple:
+        """The velocity Verlet algorithm"""
+        pos, vel, acc = x
+        newpos = pos + vel*dt + acc*(dt*dt*0.5)
+        newacc = -newpos + u/mass
+        newvel = vel + (acc+newacc)*(dt*0.5)
+        return (newpos, newvel, newacc)
+
+    def test_dyn():
+        """Test function for _dyn()"""
+        from math import cos
+        x0 = (1.0, 0.0, -1.0)
+        dt = 1e-2
+        mass = 1.0
+        tarray = [dt*t for t in range(1001)]
+        X = [x0]
+        for t in tarray[1:]:
+            xt = _dyn(X[-1], 0, dt=dt, mass=mass)
+            X.append(xt)
+        observed, _, _ = zip(*X)
+        expected = [cos(t) for t in tarray]
+        expected, observed = np.array(expected), np.array(observed)
+        plt.plot(tarray, expected, 'b-', label='expected')
+        plt.plot(tarray, observed, 'r--', label='observed')
+        plt.xlim(min(tarray), max(tarray))
+        plt.xlabel('Time')
+        plt.ylabel('Displacement from 0')
+        plt.title('Verifying the velocity Verlet algorithm')
+        plt.grid()
+        plt.legend()
+        plt.tight_layout()
+        plt.show()
     
-    Spring potential energy with Hooke's constant k = 1
-    times the l2 norm of force squared (hence quadratic)
-    cost = (0.5 * pos**2) + ||force||^2
-    """
-    C = np.array([[1, 0, 0], [0, 0, 0], [0, 0, 0]]) * 0.5
-    return x @ C @ x + u @ u
+    test_dyn()
 
+    def example_dynamics(
+            x: np.ndarray,
+            u: np.ndarray,
+            t: float,
+            dt: float = 1e-2,
+            mass: float = 1.0,
+            ) -> np.ndarray:
+        """Dynamics function
+        for the 1D simple harmonic oscillator
+        """
+        A = np.array([
+            list(_dyn((1., 0., 0.), 0., dt=dt, mass=mass)),
+            list(_dyn((0., 1., 0.), 0., dt=dt, mass=mass)),
+            list(_dyn((0., 0., 1.), 0., dt=dt, mass=mass)),
+        ]).T
+        B = np.array([
+            list(_dyn((0., 0., 0.), 1., dt=dt, mass=mass)),
+        ]).T
+        return A @ x + B @ u
 
-def _dyn(x, u, dt=1e-2, mass=1.0):
-    pos, vel, acc = x
-    newpos = pos + vel*dt + acc*(dt*dt*0.5)
-    newacc = -newpos + u/mass
-    newvel = vel + (acc+newacc)*(dt*0.5)
-    return (newpos, newvel, newacc)
+    def trajectory_without_control(
+            T: int,
+            x0: np.ndarray,
+            dt: float,
+            mass: float,
+            ) -> np.ndarray:
+        """Return the trajectory of system states
+        as an array of shape (T+1, x0.size)
+        """
+        X0 = [x0]
+        for t in range(T):
+            X0.append(
+                example_dynamics(
+                    X0[-1], np.zeros(1), t*dt,
+                    dt=dt, mass=mass,
+                )
+            )
+        X0 = np.array(X0)
+        return X0
 
-
-def test_dyn():
-    from math import cos
-    x0 = (1.0, 0.0, -1.0)
-    dt = 1e-2
-    tarray = [dt*t for t in range(1001)]
-    X = [x0]
-    for t in tarray[1:]:
-        xt = _dyn(X[-1], 0, dt=dt)
-        X.append(xt)
-    xarray, _, _ = zip(*X)
-    plt.plot(tarray, [cos(t) for t in tarray], 'r-', label='expected')
-    plt.plot(tarray, xarray, 'b:', label='observed')
-    plt.legend()
-    plt.show()
-
-
-def example_dynamics(x, u, t, dt=1e-2, mass=1.0):
-    """A dynamics function just for code verification
-
-    A simple harmonic oscillator with mass = 1 around x = 0
-    """
-    A = np.array([
-        list(_dyn((1., 0., 0.), 0., dt=dt, mass=mass)),
-        list(_dyn((0., 1., 0.), 0., dt=dt, mass=mass)),
-        list(_dyn((0., 0., 1.), 0., dt=dt, mass=mass)),
-    ]).T
-    B = np.array([
-        list(_dyn((0., 0., 0.), 1., dt=dt, mass=mass)),
-    ]).T
-    return A @ x + B @ u
-
-
-if __name__ == '__main__':
-    key = random.PRNGKey(0)
+    # Verification code
     T = 1000
     DELTA_T = 1e-1
     MASS = 1.0
@@ -76,24 +129,14 @@ if __name__ == '__main__':
         maxiter=1000,
     )
     tarray = np.arange(T+1)*DELTA_T
-    X0 = [x0]
-    for t in range(T):
-        X0.append(
-            example_dynamics(
-                X0[-1], np.zeros(1), t*DELTA_T,
-                dt=DELTA_T, mass=MASS,
-            )
-        )
-    X0 = np.array(X0)
+    X0 = trajectory_without_control(T, x0, dt=DELTA_T, mass=MASS)
     plt.figure(figsize=(14., 6.))
     plt.plot(tarray, np.zeros_like(tarray), 'k-')
     plt.plot(tarray, X0[:, 0], 'r--',
              label='trajectory without control')
     plt.plot(tarray, X[:, 0], 'b-',
              label='trajectory with optimal control')
-    # plt.plot(tarray[:-1], U0[:, 0], 'g:', label='initial control')
-    # plt.plot(tarray[:-1], U[:, 0], 'r:', label='optimal control')
-    plt.xlim(0, T*DELTA_T)
+    plt.xlim(tarray.min(), tarray.max())
     plt.xlabel('Time')
     plt.ylabel('Displacement from 0')
     plt.title(f'obj = {obj}')
@@ -101,3 +144,7 @@ if __name__ == '__main__':
     plt.legend()
     plt.tight_layout()
     plt.show()
+
+
+if __name__ == '__main__':
+    verify_ilqr()
