704aaecde3260deea0dea2787f6fbe3dd5b0ca8f
Eugene Park <jsukpark@users.noreply.github.com>
2023-12-08 03:37:57 -0600

    [WIP] Implement GPS (cont'd)
    - Still doesn't work..
    - Need to somehow access different lagrangian row each time
      - But t doesn't work as an integer variable

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """Main code
 """
 
-from typing import Any, Tuple, Callable
+from typing import Any, Tuple, Callable, Optional
 from functools import partial
 from itertools import product
 
@@ -11,7 +11,7 @@ from torch.utils.data import DataLoader
 import torch.nn.functional as F
 import gymnasium as gym
 import jax
-import jax.numpy as jnp
+from jax import numpy as jnp, vmap
 from trajax.optimizers import ilqr
 import matplotlib.pyplot as plt
 
@@ -156,30 +156,23 @@ def verify_ilqr():
     plt.show()
 
 
-def to_onehot(a: jnp.ndarray):
-    """Given a 1D array `a`,
-    return array `anew` of the same shape where
-    ```
-    anew[i] == (1.0 if i == argmax(a) else 0.0)
-    ```
+def obs_v1_to_v0(state: Tuple) -> Tuple:
+    """Given a tuple of v1 Acrobat state variables
+    (cos1, sin1, cos2, sin2, dtheta1, dtheta2),
+    return its v0 version
+    (theta1, theta2, dtheta1, dtheta2)
     """
-    assert a.ndim == 1
-    anew = (jnp.arange(a.size) == a.argmax()).astype(float)
-    return anew
+    c1, s1, c2, s2, dtheta1, dtheta2 = state
+    theta1 = np.arccos(c1)
+    if s1 < 0:
+        theta1 = -theta1
+    theta2 = np.arccos(c2)
+    if s2 < 0:
+        theta2 = -theta2
+    return theta1, theta2, dtheta1, dtheta2
 
 
-def to_onehot_p(a: jnp.ndarray):
-    """Similar to `to_onehot()`,
-    but split evenly between any ties
-    (for example, if `a = [1, 2, 3, 3]`,
-    then `anew = [0.0, 0.0, 0.5, 0.5]`)
-    """
-    anew = (a == a.max()).astype(float)
-    anew /= anew.sum()
-    return anew
-
-
-def discretize_action(a: jnp.ndarray):
+def discretize_action(a: jnp.ndarray) -> int:
     """Convert each value `a[i]` in range [-1, 1]
     to index of nearest action (in {0, 1, 2}, where
     action[i] = a[i].round() + 1)
@@ -187,68 +180,173 @@ def discretize_action(a: jnp.ndarray):
     return a.round() + 1
 
 
+def jnp_arr_to_tensor(a: jnp.ndarray) -> torch.Tensor:
+    return torch.tensor(a.tolist())
+
+
+def tensor_to_jnp_arr(t: torch.Tensor) -> jnp.ndarray:
+    return jnp.array(t.numpy())
+
+
 def guided_policy_search(
         policy: torch.nn.Module,
         cost: Callable,
         dynamics: Callable,
         env: gym.Env,
+        x0: Optional[Tuple] = None,
+        maxiter: int = 100,
+        tol: float = 1e-9,
         num_steps_pred: int = 50,
         num_epochs: int = 100,
-        convert_to_action: Callable = lambda x: x,
-        lagrange_multiplier_learning_rate: float = 1e-3,
-        ) -> None:
-    """Given an untrained parametrized policy function,
+        alpha: float = 1e-3,
+        ) -> torch.nn.Module:
+    """Given an untrained parametrized policy (a neural network),
     train the policy function parameters in-place
     using guided policy search
+    and return the trained policy
+
+    Args:
+        policy: (torch.nn.Module) x_t -> u_t (continuous var.)
+
+        cost: (Callable) (x_t, u_t) -> c_t
+
+        dynamics: (Callable) (x_t, u_t) -> x_{t+1}
+
+        env: RL environment
+
+        num_steps_pred: (int) number of time steps to predict
+
+        num_epochs: (int) number of epochs for SGD
+
+        convert_to_action: (Callable) postprocess control computed with iLQR
+        to an element in `env.action_space`
+
+        alpha: (float) Lagrange multiplier learning rate
+    
+    Returns:
+        policy: (torch.nn.Module) x_t -> u_t
     """
-    out = env.reset(seed=PRNG_SEED)
-    if isinstance(out, Tuple) and len(out) == 2 and isinstance(out[1], dict):
-        out = out[0]
-    x0 = jnp.array(out)
-    action_dim = gym.spaces.utils.flatdim(env.action_space)
-    # TODO: Ensure this is vectorizable, esp. matmul in the second term
-    def lagrangian(x, u, tidx, lambda_t):
-        """Surrogate cost"""
-        return cost(x, u, tidx) + lambda_t @ (policy(x) - convert_to_action(u))
-
-    # Step 1 (minimizing over trajectory):
-    # Find traj that minimizes Lagrangian(traj, params, lambda_) using iLQR
-    lambda_ = jax.random.normal(KEY, (num_steps_pred, action_dim))
-    # surrogate cost
-    def s_cost(x, u, tidx):
-        return lagrangian(x, u, tidx, lambda_[tidx])
-
+    if x0 is None:
+        x0 = env.reset(seed=PRNG_SEED)
+        if (isinstance(x0, Tuple) and
+                len(x0) == 2 and
+                isinstance(x0[1], dict)):
+            x0 = x0[0]
+    x0 = obs_v1_to_v0(x0)
     policy.eval()
-    X, U, _, _, _, _, _ = ilqr(
-        s_cost,
-        dynamics,
-        x0,
-        jnp.zeros((num_steps_pred, action_dim)),
-    )
-    # Step 2 (minimizing over parameters):
-    # Find params that minimizes Lagrangian(traj, params, lambda_) using SGD
-    U = jax.vmap(convert_to_action, 0)(U)
-    assert X.shape[0] == U.shape[0] + 1
-    # data = list(zip(np.array(X), np.array(U)))
-    # loader = DataLoader(data, batch_size=num_steps_pred)
-    loader = [(torch.tensor(np.array(X)), torch.tensor(np.array(U)))]
-    optimizer = torch.optim.Adam(policy.parameters(), lr=1e-3)
-    policy.train()
-    for epoch, batch in product(range(num_epochs), loader):
-        x, u = batch
-        uhat = policy(x)
-        loss = F.cross_entropy(uhat, u)
-        if loss < 1e-8:
+    action_dim = policy(torch.tensor(x0)).numel()
+    # Define Lagrangian
+    def lagrangian(x: jnp.ndarray, u: jnp.ndarray,
+                   t: jnp.ndarray, l: jnp.ndarray) -> float:
+        """Lagrangian for DGD"""
+        if x.ndim == u.ndim == 2:
+            assert t.ndim == 1 and l.ndim == u.ndim and \
+                x.shape[0] == u.shape[0] == t.shape[0] == l.shape[0]
+            return vmap(lagrangian)(x, u, t, l).sum()
+        diff: torch.Tensor = policy(jnp_arr_to_tensor(x)) - jnp_arr_to_tensor(u)
+        return cost(x, u, t) + l @ tensor_to_jnp_arr(diff)  # scalar
+    
+    lambda_ = list(jnp.zeros((num_steps_pred, action_dim)))
+
+    # TODO: Below doesn't work. Somehow need to use the correct lambda_ row but there's no way to index them.
+    @partial(jax.jit, static_argnums=2)
+    def s_cost(x, u, t):
+        """Surrogate cost"""
+        # assert isinstance(t, int), f"{type(t)} != int"
+        return lagrangian(x, u, t, lambda_[t])
+    
+    # iterative LQR loop
+    for idx in range(maxiter):
+        # Step 1 (minimizing over trajectory):
+        # Find traj that minimizes Lagrangian(traj, params, lambda_) using iLQR
+        policy.eval()
+        X, U, _, _, _, _, _ = ilqr(
+            s_cost,
+            dynamics,
+            jnp.array(x0),
+            jnp.zeros((num_steps_pred, action_dim)),
+        )
+        # Step 2 (minimizing over parameters):
+        # Find params that minimizes Lagrangian(traj, params, lambda_) using SGD
+        assert X.shape[0] == U.shape[0] + 1
+        data = list(zip(np.array(X), np.array(U)))
+        loader = DataLoader(data, batch_size=1)
+        # loader = [(torch.tensor(np.array(X)), torch.tensor(np.array(U)))]
+        optimizer = torch.optim.SGD(policy.parameters(), lr=0.01, momentum=0.9)
+        scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.9)
+        policy.train()
+        for epoch in range(num_epochs):
+            for x, u in loader:
+                optimizer.zero_grad()
+                uhat = policy(x)
+                loss = F.cross_entropy(uhat, u)
+                loss.backward()
+                optimizer.step()
+            scheduler.step()
+        # Step 3 (maximizing over Lagrange multiplier):
+        # Find lambda_ that maximizes Lagrangian(traj, params, lambda_)
+        dlambda = jax.grad(
+            partial(lagrangian,
+                    jnp_arr_to_tensor(X[:-1]),
+                    jnp_arr_to_tensor(U),
+                    jnp.arange(num_steps_pred),
+                    ),
+        )(lambda_)
+        lambda_ = lambda_ + alpha * dlambda
+        if jnp.linalg.norm(alpha * dlambda) < tol:
             break
-        loss.backward()
-        optimizer.step()
-        optimizer.zero_grad()
-    # Step 3 (maximizing over Lagrange multiplier):
-    # Find lambda_ that maximizes Lagrangian(traj, params, lambda_)
-    dlambda = jax.grad(
-        partial(lagrangian, X[:-1], U, jnp.arange(num_epochs)),
-    )
-    lambda_ = lambda_ + lagrange_multiplier_learning_rate * jax.grad(lambda_)
+    return policy
+
+
+def cost(x, u, t) -> float:
+    """Cost function
+
+    Args:
+        x: [theta1, theta2, dtheta1, dtheta2]
+        u: [torque] (torque \in [-1, 1])
+        t: int (time index)
+    """
+    cosines = jnp.cos(jnp.array([[1., 0., 0., 0.],
+                                 [1., 1., 0., 0.]]) @ x)
+    return max(0., 1. - jnp.array([1., 1.]) @ cosines) + (u @ u)
+
+
+def dynamics(env, x, u, t) -> jnp.ndarray:
+    """Dynamics function"""
+    return env.step(discretize_action(u))[0]
+
+
+class MyPolicy:
+    def __init__(self):
+        self.policy = torch.nn.Sequential(
+            torch.nn.Linear(4, 4),
+            torch.nn.ReLU(),
+            torch.nn.Linear(4, 1),
+            torch.nn.Tanh(),
+        )
+        self.tstep = 0
+    
+    def __call__(self, env, obs):
+        if self.tstep % 50 == 0:
+            self.policy = guided_policy_search(
+                self.policy,
+                cost,
+                dynamics,
+                init_env(),
+                obs,
+            )
+        act = self.policy(torch.tensor(obs_v1_to_v0(obs))).numpy()
+        assert act.shape == (1,)
+        act = discretize_action(act)[0]
+        self.tstep += 1
+        return act
+    
+select_action = MyPolicy()
 
 if __name__ == '__main__':
-    pass
+    policy = torch.nn.Sequential(
+        torch.nn.Linear(4, 4),
+        torch.nn.ReLU(),
+        torch.nn.Linear(4, 1),
+        torch.nn.Tanh(),
+    )
